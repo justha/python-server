6868d8b15a87b5768e01260b41602e2603df767a
justha <nganha42@gmail.com>
2020-10-09 11:52:01 -0500

    can add/update animals, employees and locations

--- employees/request.py
+++ employees/request.py
@@ -34,5 +34,22 @@ def get_single_employee(id):
     return requested_employee
 
 
+def create_employee(employee):
+    max_id = EMPLOYEES[-1]["id"]
+
+    new_id = max_id + 1
+
+    employee["id"] = new_id
+
+    EMPLOYEES.append(employee)
+
+    return employee
+    
+    
+def update_employee(id, new_employee):
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            EMPLOYEES[index] = new_employee
+            break
     
     
